b3040513a400cfc3fc31fca6185252e1f173a9b6
TungOscar <a9825434@gmail.com>
2023-06-07 20:10:33 +0800

    cccc

---
+++ web411018654final/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web411018654final.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Music(string name, int number)
+        {
+            if (number==1)
+            {
+                ViewBag.number = "請輸入姓名";
+            }
+
+            return View();
+        }
+    }
+}
